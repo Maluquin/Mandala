87e3e6d35a9469c6e88d651044acf2e3cf870ee0
Maluquin <93450411+Maluquin@users.noreply.github.com>
2023-09-28 16:49:57 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './navigation/bottomTabNavigator';
+import MyStack from './navigation/stackNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator/>
+    </NavigationContainer>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
